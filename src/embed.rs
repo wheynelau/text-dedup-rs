6d94ca5fdad1f9acf71600a46f139fce3d8f65fd
wheynelau <waynelau15045@gmail.com>
2024-06-19 15:55:00 +0800

    fix deps and remove bin

--- src/embed.rs
+++ src/embed.rs
@@ -148,23 +148,6 @@ pub fn py_embed_func(text: &str, permutations: (ArcArray1<u64>, ArcArray1<u64>),
     hs
 }
 
-#[allow(dead_code)]
-fn main() {
-    let text = "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?".to_string();
-
-    // These functions are fixed for all iterations
-    let num_perm = 200;
-    let (b, r) = (33,6);
-    let permutations = generate_permutations(MODULE_PRIME as usize, num_perm);
-    let hash_ranges: Vec<(i32,i32)> = (0..b)
-                    .map(|i| (i*r, (i+1)*r))
-                    .collect();
-
-    for _ in 0..100000 {
-        py_embed_func(&text, permutations.clone() ,hash_ranges.clone());
-    }
-    // py_embed_func(&text, hash_ranges.clone());
-}
 #[cfg(test)]
 mod tests {
     use super::*;
